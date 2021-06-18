4c80c90d090630ad8b201a0ffd14df557c973344
SafaanHashmi <55549354+SafaanHashmi@users.noreply.github.com>
2021-06-18 12:34:59 +0530

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Commerce Soluions Indore</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/title-icon-CS.jpg" rel="icon">
+  <link href="assets/img/apple-touch-icon.jpg" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+ 
+  <style>
+	@media (max-width: 750px) {
+	  .hide-when-narrow {
+		display: none;
+	  }
+	}
+	@media (min-width: 751px) {
+	  .hide-when-wide {
+		display: none;
+	  }
+	}
+  </style>
+  
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+		<!--style="text-decoration: underline;text-decoration-color: black;text-decoration-line: overline underline;"      additional-->
+      <h1 class="logo mr-auto"><a href="index"><span class="hide-when-narrow">commerce SOLUTIONS</span> <span class="hide-when-wide">C.S.</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li class="active"><a href="blog.php">Blogs</a></li>
+          <li><a href="youtube.php">Youtube</a></li>
+          <li><a href="faq.php">FAQ and Docs</a></li>
+        <!--  <li><a href="events.html">Events</a></li>
+          <li><a href="pricing.html">Pricing</a></li>
+          <li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Deep Drop Down</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li> -->
+          <li><a href="contact.php">Contact Us</a></li>
+		  <li><a href="about">About</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+     <!-- <a href="courses.html" class="get-started-btn">Get Started</a> -->
+
+    </div>
+  </header><!-- End Header -->
+  
+<br><br>
+<section id="events" class="events">
+      <div class="container" data-aos="fade-up">
+	  
+		<div class="section-title">
+          <h2>Have a look at our</h2>
+          <p>Blogs</p>
+        </div>
+		
+        <div class="row">
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/blog 1 accon.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Introduction to Accountancy</a></h5>
+                <p class="font-italic text-center">Sunday, November 26th 2020 at 7:00 pm</p>
+                <p class="card-text">Accounting is the language of business. It is the system of recording, summarizing, and analyzing an economic entity's financial transactions. 
+				Effectively communicating this information is key to the success of every business.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/blog 2 gstcopy.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Accountiong for GST</a></h5>
+                <p class="font-italic text-center">Sunday, October 15th 2020 at 5:00 pm</p>
+                <p class="card-text">A blog about all Accounts and Records Under GST. It helps understand terms like Accounting entries under GST, Electronic Cash and Credit Ledger, 
+				Period for Retention of Accounts under GST</p>
+              </div>
+            </div>
+		  </div>
+        </div>
+
+		<div class="row">
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/blog 3 jr.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Journal Entries</a></h5>
+                <p class="font-italic text-center">Sunday, September 28th 2020 at 9:00 pm</p>
+                <p class="card-text">A journal entry is the act of keeping or making records of any transactions either economic or non-economic. Transactions are listed in an accounting journal
+				that shows a company's debit and credit balances.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/blog 4 db.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Rules of Debit and Credit</a></h5>
+                <p class="font-italic text-center">Sunday, September 15th 2020 at 7:00 pm</p>
+                <p class="card-text">A debit increases asset or expense accounts, and decreases liability, revenue or equity accounts. A credit is always positioned on the right side of an entry. 
+				It increases liability, revenue or equity accounts and decreases asset or expense accounts.</p>
+              </div>
+            </div>
+		  </div>
+        </div>
+
+      </div>
+    </section><!-- End Events Section -->
+
+
+
+
+
+<?php
+	require("footer.php");
+?>   
